3b60d52a6a3bc7f7c11c1e76f0948af52e601ce1
Alessio Massetti <alessiomassetti@gmail.com>
2015-12-18 12:55:09 +0100

    shout randomize

--- responders/shout.js
+++ responders/shout.js
@@ -5,7 +5,12 @@ var shoutPattern = /^shout/i
  * A simple responder. Listens for "shout <phrase>", and shouts it back.
  */
 var shout = function(message,callback){
-  callback(null, message.replace(shoutPattern,'').toUpperCase())
+	var test = Math.random();
+    if(test < 0.5) {
+  		callback(null, message.replace(shoutPattern,'').toUpperCase())
+  	else {
+  		callback(null,'non sono il tuo schiavo')
+  	}
 }
 
 shout.match = shoutPattern
