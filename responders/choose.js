0f1d36ec664004b40706dc2095c8c01d18a020ce
Alessio Massetti <alessiomassetti@gmail.com>
2016-04-22 22:53:20 +0200

    Fix

--- responders/choose.js
+++ responders/choose.js
@@ -4,10 +4,11 @@ var shoutPattern = /^choose/i
 /**
  * A simple responder. Listens for "shout <phrase>", and shouts it back.
  */
-var shout = function(message,callback){
-	var choices = message.trim().split(",");
-	var choosen = Math.floor((Math.random() * *len(choices)) + 1);
-	callback(null, choices[choosen])
+var choose = function(message,callback){
+	var choices = message.trim().split(":");
+	choices = choices[1].trim().split(",");
+	var choosen = Math.floor((Math.random() * choices.length));
+	callback(null, choices[choosen]);
 }
 
 choose.match = shoutPattern
